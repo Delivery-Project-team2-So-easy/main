c89172f6905174962c5363ec606e660b4d9bae5f
Sung min Park <somemen1234@naver.com>
2023-07-19 14:07:58 +0900

    feat : add store like feature
    - 매장 좋아요를 누르는 부분 기능을 추가
    - like repository파일을 추가해서 likes table을 활용하는 부분은 여기서
      처리가 되도록 수정

---
+++ repositories/like.repository.js
@@ -0,0 +1,38 @@
+const { Store_like, Review_like } = require('../models');
+
+class LikeRepository {
+  existUserLike = async (userId, storeId) => {
+    const existLike = await Store_like.findOne({ where: { user_id: userId, store_id: storeId } });
+    return existLike;
+  };
+
+  createUserLike = async (userId, storeId) => {
+    await Store_like.create({ user_id: userId, store_id: storeId });
+    return;
+  };
+
+  deleteUserLike = async (userId, storeId) => {
+    await Store_like.destroy({ where: { user_id: userId, store_id: storeId } });
+    return;
+  };
+
+  likeReview = async (userId, reviewId) => {
+    const duplicateCheck = await Review_like.findOne({
+      where: {
+        [Op.and]: [{ user_id: userId }, { review_id: reviewId }],
+      },
+    });
+
+    if (duplicateCheck) {
+      await duplicateCheck.destroy();
+      const likeCount = await Review_like.count({ where: { review_id: reviewId } });
+      return { message: '좋아요 취소', likeCount };
+    }
+
+    await Review_like.create({ user_id: userId, review_id: reviewId });
+    const likeCount = await Review_like.count({ where: { review_id: reviewId } });
+    return { message: '좋아요 완료', likeCount };
+  };
+}
+
+module.exports = LikeRepository;
--- routes/user.route.js
+++ routes/user.route.js
@@ -3,7 +3,12 @@ const router = express.Router();
 const UserController = require('../controllers/user.controller');
 const userController = new UserController();
 
+const authMiddleware = require('../middlewares/auth-middleware');
+
 router.post('/users/signup', userController.signUp);
 router.post('/users/login', userController.login);
+router.post('/users/logout', authMiddleware, userController.logout);
 router.post('/users/checkEmail', userController.checkEmail);
+router.post('/user/store/:storeId/like', authMiddleware, userController.storeLike);
+
 module.exports = router;
