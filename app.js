a7811b94ae2c5f3213ca8d05fe3184865b21bf5a
leeseungjun <cjstkrak@gmail.com>
2023-07-21 14:36:02 +0900

    feat: 로그인페이지 구현 회원가입페이지 진행중

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ db.sequelize.sync({});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(express.static('assets')); //정적파일 사용하기 위해, assets의 html, css, js, 이미지 등
 app.use('/', [userRouter, storeRouter, reviewRouter, orderRouter]);
 
 app.use((err, req, res, next) => {
