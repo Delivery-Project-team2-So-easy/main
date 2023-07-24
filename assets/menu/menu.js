769f86196027f9705a2e2b06b0de21355ce8f262
Lee SangHun <mysh1105@gmail.com>
2023-07-24 10:58:00 +0900

    style : change menu style

--- assets/menu/menu.js
+++ assets/menu/menu.js
@@ -30,6 +30,7 @@ window.addEventListener('DOMContentLoaded', async () => {
           let menu_price = menu.price;
           let menu_option = menu.option;
 
+          menu_img === null ? (menu_img = '../images/noimage.png') : menu_img;
           let temp_html = `
         <div class="menu-box" id="${menu_id}">
           <img
